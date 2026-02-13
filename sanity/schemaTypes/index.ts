3756a4273244e6c6e45d5d26a35d53dc8e42746d
mahadihasandev <mayhaydihasan.com@gmail.com>
2026-02-14 00:03:23 +0600

    varcle deploy fix

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,15 +1,26 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { CategoryType } from './CategoryType'
-import { AddressType } from './AddressType'
-import { AuthorType } from './AuthorType'
-import { BlockContentType } from './BlockContentType'
-import { BlogCategoryType} from './BlogCategoryType'
-import { BlogType } from './BlogType'
-import { BrandType } from './brandType'
-import { ProductType } from './ProductType'
-import { BannerType } from './Bannertype'
-import { OrderType } from './OrderType'
+import { type SchemaTypeDefinition } from "sanity";
+import { CategoryType } from "./CategoryType";
+import { AddressType } from "./AddressType";
+import { AuthorType } from "./AuthorType";
+import { BlockContentType } from "./BlockContentType";
+import { BlogCategoryType } from "./BlogCategoryType";
+import { BlogType } from "./BlogType";
+import { ProductType } from "./ProductType";
+import { BannerType } from "./Bannertype";
+import { OrderType } from "./OrderType";
+import { BrandType } from "./brandType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [CategoryType,AddressType,AuthorType,BlockContentType,BlogCategoryType,BlogType,BrandType,ProductType,BannerType,OrderType],
-}
+  types: [
+    CategoryType,
+    AddressType,
+    AuthorType,
+    BlockContentType,
+    BlogCategoryType,
+    BlogType,
+    BrandType,
+    ProductType,
+    BannerType,
+    OrderType,
+  ],
+};
