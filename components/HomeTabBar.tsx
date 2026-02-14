db45775db188b734c7fe372351e3bd7c732e9bee
mahadihasandev <mayhaydihasan.com@gmail.com>
2026-02-14 08:14:26 +0600

    see all button fix

--- components/HomeTabBar.tsx
+++ components/HomeTabBar.tsx
@@ -12,12 +12,12 @@ const HomeTabBar = ({selectedTab,onTabSelect}:Props) => {
         <div className='flex items-center gap-3 font-semibold text-sm'>
             {
                 productType.map((item)=>(
-                    <button onClick={()=>(onTabSelect(item?.title))} className={`border font-poppins border-shop_light_blue/30 px-4 py-1.5 md:px-6 md:py-2 hover:bg-shop_light_blue hover:border-shop_light_blue rounded-full hover:text-white hoverEffect 
+                    <button onClick={()=>(onTabSelect(item?.title))} className={`border font-poppins border-shop_light_blue/30 px-3 py-1.5 md:px-6 md:py-2 hover:bg-shop_light_blue hover:border-shop_light_blue rounded-full hover:text-white hoverEffect 
                         ${selectedTab==item?.title?"bg-shop_light_blue text-white border-shop_light_blue":"bg-shop_light_blue/25"}`} key={item?.title}>{item?.title}</button>
                 ))
             }
         </div>
-        <Link className='border font-poppins border-shop_light_blue/25 px-4 py-1.5 md:px-6 md:py-2 hover:bg-shop_light_blue hover:border-shop_light_blue rounded-full hover:text-white hoverEffect' href={'/'}>See all</Link>
+        <Link className='border font-poppins border-shop_light_blue/25 px-4 py-1.5 md:px-6 md:py-2 hover:bg-shop_light_blue hover:border-shop_light_blue rounded-full hover:text-white hoverEffect' href={'/shop'}>See all</Link>
     </div>
   )
 }
